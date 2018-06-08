7aa03255bec7e7dd63fe7a362acaeded51f586db
Rui Maximo <rui@maximo.ws>
2018-06-08 07:26:50 -0700

    update TestCrowdsale

--- contracts/TestCrowdsale.sol
+++ contracts/TestCrowdsale.sol
@@ -3,22 +3,29 @@ pragma solidity ^0.4.13;
 import "./Crowdsale.sol";
 
 contract TestCrowdsale is Crowdsale {
-  function TestCrowdsale() public {
-    presaleDate = 1530932400;   
-    saleDate = presaleDate + 32 days;
-    endDate = saleDate + 32 days;
+  constructor() public {
+    presaleDate = now + 15 minutes;
+    saleDate = presaleDate + 1 hours;
+    endDate = saleDate + 1 hours;
 
-    state currentState = state.presaleStart;
+    saleState = state.pendingStart;
 
-    ethToTokenConversion = 1;
+    ethToTokenConversion = 26950;                 // 1 ETH == 26,950 HighVibe tokens
 
-    maxTokenSupply = 10.3 ether;
-    companyTokens = 5 ether;             // allocation to company, private presale and users (marketing)
+    maxTokenSupply = 2400000000 ether;            // 2,400,000,000 tokens
+    companyTokens = 600000000 ether;              // 20% company reserve, 20% team + advisors, 10% contributors + authors
 
-    uint maxCommunityWithoutBonusCap = 2 ether;
-    uint maxCommunityCap = 2.3 ether;
-    uint maxCrowdsaleCap = 3 ether;
+    maxPresaleWithoutBonus = 1011739130 ether;
+    maxPresaleCap = 1200000000 ether;             // tokens allocated to pre-sale
+    tier1 = 240000000 ether;                      // allocated tokens for this tier (bonus tokens not included)
+    tier2 = 250000000 ether;                      // allocated tokens for this tier (bonus tokens not included)
+    tier3 = 521739130 ether;                      // allocated tokens for this tier (bonus tokens not included)
 
-    maxContribution = 1 ether;                  // maximum contribution during community round
+    maxSaleWithoutBonus = 1137511326 ether;
+    maxSaleCap = 1200000000 ether;                // tokens allocated to main sale 
+    tier4 = 272727273 ether;                      // allocated tokens for this tier (bonus tokens not included)
+    tier5 = 279069767 ether;                      // allocated tokens for this tier (bonus tokens not included)
+    tier6 = 285714286 ether;                      // allocated tokens for this tier (bonus tokens not included)
+    tier7 = 300000000 ether;
   }
 }
